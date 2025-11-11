3341c5db6e48cb0203ac35d1dd5e3ec87a91dc9a
ArtemShpv <artem.shapoval99@gmail.com>
2025-11-11 13:00:42 +0200

    Solution

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,20 @@
-export const Tabs = () => {};
+import React from 'react';
+
+export const Tabs = ({ tabs, activeTabId, onTabSelected }) => (
+  <div className="tabs is-boxed" data-cy="TabsComponent">
+    <ul>
+      {tabs.map(tab => (
+        <button
+          type="button"
+          className={activeTabId === tab.id ? 'is-active' : ''}
+          data-cy="Tab"
+          onClick={() => onTabSelected(tab.id)}
+        >
+          <a href={`#${tab.id}`} data-cy="TabLink">
+            {tab.title}
+          </a>
+        </button>
+      ))}
+    </ul>
+  </div>
+);
